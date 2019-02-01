e582ecd1b9768b4b053632d244bf217641ce4d89
Lalith kaumar <47214998+lalithramesh@users.noreply.github.com>
2019-02-01 00:42:23 -0800

    Update vowelsconsonent.py

--- vowelsconsonent.py
+++ vowelsconsonent.py
@@ -1,5 +1,5 @@
 lal=raw_input()
 if (lal=='a' or lal=='A' or lal=='e' or lal=='E' or lal=='i' or lal=='I' or lal=='o' or lal=='O' or lal=='u' or lal=='U'):
-     print("vowel")
+     print("Vowel")
 else:
-     print("consonent")
+     print("Consonent")
