5dce264642651f3894b8eea1b618339c259d46b3
Lalith kaumar <47214998+lalithramesh@users.noreply.github.com>
2019-02-01 00:39:44 -0800

    Create vowelsconsonent.py

---
+++ vowelsconsonent.py
@@ -0,0 +1,5 @@
+lal=raw_input()
+if (lal=='a' or lal=='A' or lal=='e' or lal=='E' or lal=='i' or lal=='I' or lal=='o' or lal=='O' or lal=='u' or lal=='U'):
+     print("vowel")
+else:
+     print("consonent")
